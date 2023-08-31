4d21b5a650e20b272956ce43d3b3213701814239
baydanemre <baydaniemre@gmail.com>
2023-08-31 17:40:58 +0300

    Python Basics -ATM App

--- Small Python Projects/ATMApp.py
+++ Small Python Projects/ATMApp.py
@@ -1,32 +1,37 @@
 amountinbank = 10000
+isExited = False
+
 print("WELCOME TO MY ATM")
 print("Deposit : 1")
 print("Withdraw : 2")
 print("Your Amount: 3")
 print("Exit : 4")
 
-selection = input("Please Enter a number: ")
+while(isExited == False):
 
-if(selection == "1"):
-    amountDeposited = int(input("Enter the amount: "))
-    amountinbank += amountDeposited
-    print("Your new amount: ",amountinbank)
+    selection = input("Please Enter a number: ")
 
-elif(selection == "2"):
-    amountWithdrawed = int(input("Enter the amount: "))
-    if(amountWithdrawed > amountinbank):
-        print("Transaction is invalid")
-    else:
-        amountinbank -= amountWithdrawed
+    if(selection == "1"):
+        amountDeposited = int(input("Enter the amount: "))
+        amountinbank += amountDeposited
         print("Your new amount: ",amountinbank)
 
-elif(selection == "3"):
-    print("Your amount:",amountinbank)
+    elif(selection == "2"):
+        amountWithdrawed = int(input("Enter the amount: "))
+        if(amountWithdrawed > amountinbank):
+            print("Transaction is invalid")
+        else:
+            amountinbank -= amountWithdrawed
+            print("Your new amount: ",amountinbank)
 
-elif(selection == "4"):
-    print("Exited.")
+    elif(selection == "3"):
+        print("Your amount:",amountinbank)
 
-else:
-    print("Enter a valid transaction")
+    elif(selection == "4"):
+        print("Exited.")
+        isExited = True
+
+    else:
+        print("Enter a valid transaction")
 
 
